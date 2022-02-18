cadc81f5e5a95e507dc943bf73158165e3200d96
Hahjir Alsamach <al-samach.hahjir@student.greenriver.edu>
2022-02-17 22:16:32 -0500

    added data-layer and validation functions

---
+++ model/data-layer.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function indoorInterests()
+{
+    return array('tv', 'movies', 'cooking', 'board games', 'puzzles', 'reading', 'playing cards', 'video games');
+}
+
+
+function outdoorInterests()
+{
+    return array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing');
+}
