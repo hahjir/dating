9d783045936fb4c968491494642e8ee82c925f3e
Hahjir Alsamach <al-samach.hahjir@student.greenriver.edu>
2022-03-05 16:00:04 -0500

    added class and made static

---
+++ model/validation.php
@@ -0,0 +1,59 @@
+<?php
+
+class Validation
+{
+
+//validates name
+    static function validName($name): bool
+    {
+        return strlen($name) >= 1;
+    }
+
+//validate age
+    static function validAge($age)
+    {
+        return $age >= 18 && $age <= 118;
+    }
+
+//validates phone
+    static function validPhone($phone): bool
+    {
+        return is_numeric($phone);
+    }
+
+//validates email
+    static function validEmail($email): bool
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+        return false;
+    }
+
+
+//validates indoor interests
+    static function validIndoor($interests): bool
+    {
+        $validIndoor = DataLayer::indoorInterests();
+
+        foreach ($interests as $selection) {
+            if (!in_array($selection, $validIndoor)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+//validates outdoor interests
+    static function validOutdoor($interests): bool
+    {
+        $validOutdoor = DataLayer::outdoorInterests();
+
+        foreach ($interests as $selection) {
+            if (!in_array($selection, $validOutdoor)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
