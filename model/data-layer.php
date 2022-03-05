b803129a4396b19f1de406e158245e3526176065
Hahjir Alsamach <al-samach.hahjir@student.greenriver.edu>
2022-03-05 15:58:55 -0500

    made functions static

--- model/data-layer.php
+++ model/data-layer.php
@@ -2,13 +2,13 @@
 
 class DataLayer
 {
-    function indoorInterests()
+    static function indoorInterests()
     {
         return array('tv', 'movies', 'cooking', 'board games', 'puzzles', 'reading', 'playing cards', 'video games');
     }
 
 
-    function outdoorInterests()
+    static function outdoorInterests()
     {
         return array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing');
     }
